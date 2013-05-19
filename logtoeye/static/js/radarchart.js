dcd49254eb824739c2de751f012142047ab3c93e
lwz7512 <lwz7512@gmail.com>
2013-05-19 22:44:12 +0800

    radar chart rotated arcs...

--- logtoeye/static/js/radarchart.js
+++ logtoeye/static/js/radarchart.js
@@ -3,12 +3,16 @@
 function JSAnimationEngine (canvas) {
 	window.JSEngineChildren = [];
 	window.JSEngineFlag = true;
+        window.JSEngineCounter = 0;
 	window.JSEngineContext = canvas.getContext('2d');
 	window.JSEngineEnterframe = function enterFrame () {		
 		
 		if(!this.JSEngineFlag) return;
 		window.requestAnimationFrame(JSEngineEnterframe, canvas);//request to redraw
 			
+		this.JSEngineCounter += 1;
+
+		if(this.JSEngineCounter % 2) return;
 		this.JSEngineContext.clearRect(0, 0, canvas.width, canvas.height);//***clear all before redraw...	
 		for(var i in this.JSEngineChildren){
 			this.JSEngineChildren[i].draw(this.JSEngineContext);
@@ -37,22 +41,63 @@ function RadarChart (canvas, radius) {
     this.angle = 0;
     this.speed = 0.01;
     this.centerX = canvas.width/2;
-    this.centerY = canvas.height/2;
-    this.line = new Line(this.centerX, this.centerY, 2*radius, radius, '#00FF00');
-    this.ball = new Ball(radius, '#000000', '#0000FF');
-    this.ball.x = this.centerX;
-    this.ball.y = this.centerY;
+    this.centerY = canvas.height/2;            
+    this.ctx = canvas.getContext('2d');
+    this.ctx.translate(radius, radius);
 }
 
-RadarChart.prototype.draw = function (context) {    
+RadarChart.prototype.draw = function (ctx) {    
 
-    this.angle -= this.speed;
-    this.line.endX = this.centerX + Math.sin(this.angle+Math.PI/2) * this.radius;
-    this.line.endY = this.centerY + Math.cos(this.angle+Math.PI/2) * this.radius;
+    this.angle += this.speed;       
+    
+    ctx.save();
+    
+    ctx.clearRect(0, 0, this.radius, this.radius);
+    ctx.clearRect(0, 0, this.radius, -this.radius);
+    ctx.clearRect(0, 0, -this.radius, -this.radius);
+    ctx.clearRect(0, 0, -this.radius, this.radius);
+    ctx.rotate(this.angle);
+    
+    ctx.arc(0, 0, this.radius, 0, (Math.PI * 2), true);
+    ctx.fillStyle = "#000000";
+    ctx.fill();    
 
-    this.ball.draw(context);
-    this.line.draw(context);
+    for(var i = 0; i< 17; i++){
+	var color = "#00FF00";
+	ctx.beginPath();
+	//x, y, radius, start_angle, end_angle, anti-clockwise
+	ctx.arc(0, 0, this.radius, 0, -Math.PI/72, true);
+	ctx.lineTo(0, 0);//up side
+	ctx.lineTo(this.radius, 0);//down side
+	ctx.fillStyle = shadeColor(color, -6*i);
+	ctx.fill();
+
+	ctx.rotate(-Math.PI/72+Math.PI/180);
+    }
+
+    ctx.restore();
 };
+
+function shadeColor(color, percent) {
+
+    var R = parseInt(color.substring(1,3),16)
+    var G = parseInt(color.substring(3,5),16)
+    var B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;  
+    B = (B<255)?B:255;  
+
+    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
+}
 
 function Ball (radius, color, borderColor) {
   if (radius === undefined) { radius = 40; }
